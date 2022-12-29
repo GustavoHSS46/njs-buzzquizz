34aac2b4ec10199c5b48cd584ada8e971f7dc37f
GustavoHSS46 <mainsennamid@gmail.com>
2022-12-29 19:42:23 -0300

    version 0.25

--- src/script.js
+++ src/script.js
@@ -26,16 +26,28 @@ function displayData(res) {
     }
 }
 
+function telaInicial() {
+    page1.classList.remove("hide")
+}
+
 function criarButton() {
     page1.classList.add("hide")
     page2.classList.remove("hide")
 }
+
+
+function perguntas() {
+    page2.classList.add("hide")
+    page2Perguntas.classList.remove("hide")
+}
+
 
 const url = `https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes`
 const container = document.querySelector('.AllQuiz')
 const card = document.querySelector('.macaco') 
 const page1 = document.querySelector('.Page-1')
 const page2 = document.querySelector('.Page-2')
+const page2Perguntas = document.querySelector('.Page-2_Perguntas')
 
 
 axios
